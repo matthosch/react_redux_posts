71081091fa2c4e47580f53279aaad7bfd70a9254
Matt Hosch <matt_hosch@hotmail.com>
2020-11-25 14:19:34 -0600

    Removed comment

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,7 +1,6 @@
 const usersReducer = (state = [], action) => {
   switch (action.type) {
     case "FETCH_USER":
-      // console.log("hit");
       return [...state, action.payload];
     default:
       return state;
